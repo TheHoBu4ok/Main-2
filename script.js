a06230811c4a2448bc7af5603663f9887fff8a99
Max <example@gmail.com>
2020-12-19 21:05:04 +0500

    Array iteration methods

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
 'use strict';
 
-fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: "POST",
-    body: JSON.stringify({name: 'Alex'}),
-    headers: {
-        'Content-type': 'application/json'
-    }
-})
-.then(response => response.json())
-.then(json => console.log(json));
+// filter
+
+// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
+
+// const shortNames = names.filter(function(name) {
+//     return name.length < 5;
+// });
+
+// console.log(shortNames);
+
+// map
+
+// const answers = ['IvAn', 'AnnA', 'Hello'];
+
+// const result = answers.map(item => item.toLowerCase());
+
+// console.log(result);
+
+// every/some // some - хотя бы 1 элемент условию // every - если все элементы, подходят под условие
+
+// const some = [4, 'dsf', 'sdgsdg'];
+
+// // console.log(some.some(item => typeof(item) === 'number'));
+
+// console.log(some.every(item => typeof(item) === 'number'));
+
+// reduce
+
+// const arr = [4, 5, 1, 3, 2, 6];
+//                     //  3       4
+//                     //  4       5
+//                     //  9       1
+// const res = arr.reduce((sum, current) => sum + current, 3);
+// console.log(res);
+
+// const arr = ['apple', 'pear', 'plum'];
+            
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+// console.log(res);
+
+const obj = {
+    'Ivan': 'persone',
+    'ann:': 'persone',
+    'dog': 'animal',
+    'cat': 'animal',
+
+};
+
+const newArr = Object.entries(obj)
+.filter(item => item[1] === 'persone')
+.map(item => item[0]);
+
+console.log(newArr);
+
