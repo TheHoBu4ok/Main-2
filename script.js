8ac2cb4a7796b168c7199ac89cbbe1e73dc76789
Max <example@gmail.com>
2020-12-19 15:02:22 +0500

    Fetch

--- script.js
+++ script.js
@@ -1,54 +1,11 @@
 'use strict';
 
-// console.log('Запрос данных...');
-
-// const req = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         console.log('Подготовка данных...');
-    
-//         const product = {
-//             name: 'TV',
-//             price: 2000,
-//         };
-
-//         resolve(product);
-//     }, 2000);
-// });
-
-// // положительный исход промиса
-// req.then((product) => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             product.status = 'order';
-//             resolve(product);
-//         }, 2000);
-//     });
-// }).then((data) => {
-//     data.modify = true;
-//     return data;
-// }).then(data => {
-//     console.log(data);
-// }).catch(() => {
-//     console.error('Произошла ошибка');
-// }).finally(() => {
-//     console.log('finally');
-// });
-
-const test = time => {
-    return new Promise(resolve => {
-        setTimeout(() => resolve(), time);
-    });
-};
-
-// test(1000).then(() => console.log('1000 ms'));
-// test(2000).then(() => console.log('2000 ms'));
-
-// Promise как глоб. объект, all используется для проверки, что все промисы уже выполнились
-// Promise.all([test(1000)], test(2000)).then(() => {
-//     console.log('All');
-// });
-
-// Race выполняет действия, сразу же после того, как первый промис отработал правильно
-Promise.race([test(1000)], test(2000)).then(() => {
-    console.log('All');
-});
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: "POST",
+    body: JSON.stringify({name: 'Alex'}),
+    headers: {
+        'Content-type': 'application/json'
+    }
+})
+.then(response => response.json())
+.then(json => console.log(json));
