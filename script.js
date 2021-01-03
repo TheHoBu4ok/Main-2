df6a381a10a87ced0609d9dfb6a52caccffb8581
Max <example@gmail.com>
2021-01-03 23:43:15 +0500

    Local Storage

--- script.js
+++ script.js
@@ -1,56 +1,44 @@
 'use strict';
 
-// filter
+// localStorage.setItem('number', 5);
 
-// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
+// // localStorage.removeItem('number');
+// localStorage.clear();
 
-// const shortNames = names.filter(function(name) {
-//     return name.length < 5;
-// });
+// console.log(localStorage.getItem('number'));
 
-// console.log(shortNames);
+const checkbox = document.querySelector('#checkbox'),
+      form = document.querySelector('form'),
+      change = document.querySelector('#color');
 
-// map
+if (localStorage.getItem('isChecked')) {
+    checkbox.checked = true;
+}
 
-// const answers = ['IvAn', 'AnnA', 'Hello'];
+if (localStorage.getItem('bg') === 'changed') {
+    form.style.backgroundColor = 'red';
+}
 
-// const result = answers.map(item => item.toLowerCase());
+checkbox.addEventListener('change', () => {
+    localStorage.setItem('isChecked', true);
+});
 
-// console.log(result);
+change.addEventListener('click', () => {
+    if (localStorage.getItem('bg') === 'changed') {
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = '#fff';    // белый цвет
+    } else {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = 'red';
+    }
+});
 
-// every/some // some - хотя бы 1 элемент условию // every - если все элементы, подходят под условие
-
-// const some = [4, 'dsf', 'sdgsdg'];
-
-// // console.log(some.some(item => typeof(item) === 'number'));
-
-// console.log(some.every(item => typeof(item) === 'number'));
-
-// reduce
-
-// const arr = [4, 5, 1, 3, 2, 6];
-//                     //  3       4
-//                     //  4       5
-//                     //  9       1
-// const res = arr.reduce((sum, current) => sum + current, 3);
-// console.log(res);
-
-// const arr = ['apple', 'pear', 'plum'];
-            
-// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
-// console.log(res);
-
-const obj = {
-    'Ivan': 'persone',
-    'ann:': 'persone',
-    'dog': 'animal',
-    'cat': 'animal',
-
+const persone = {
+    name: 'Alex',
+    age: 25
 };
 
-const newArr = Object.entries(obj)
-.filter(item => item[1] === 'persone')
-.map(item => item[0]);
+const serializedPersone = JSON.stringify(persone);
+localStorage.setItem('alex', serializedPersone);
 
-console.log(newArr);
-
+console.log(JSON.parse(localStorage.getItem('alex')));
