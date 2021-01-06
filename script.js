50127b77b7227d2e53e4d5f1c3679d4bd0edd557
Max <example@gmail.com>
2021-01-06 20:12:03 +0500

    Get and set

--- script.js
+++ script.js
@@ -1,35 +1,19 @@
 'use strict';
 
-// new RegExp('pattern', 'flags');
-// /pattern/f
+// get(геттер) и set(сеттер) - свойства аксессоры
 
-// const ans = prompt('Введите ваше число');
+const persone = {
+    name: "Alex",
+    age: 25,
 
-// const reg = /\d/g;
-// console.log(ans.match(reg));
+    get userAge() {
+        return this.age;
+    },
 
-const str = 'My name is R2D2';
+    set userAge(num) {
+        this.age = num;
+    }
+};
 
-console.log(str.match(/\W/ig));
-
-// \D не числа
-// \W не буквы
-
-// \d поиск цифр
-// \w поиск слов
-// \s поиск всех пробелов
-
-// test() - true or false
-// i - не зависит от регистра
-// g - глобальный изет все совпадения
-// m - поиск в нескольких строках
-
-// console.log(ans.search(reg));
-// console.log(ans.match(reg));
-
-// const pass = prompt('Password'); 
-
-// . означает все символы в выражении
-// console.log(pass.replace(/./g, '*'));
-
-// console.log('12-34-56'.replace(/-/g, ':'));
+console.log(persone.userAge = 30);
+console.log(persone.userAge);
