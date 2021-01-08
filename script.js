e6fa991185371141612bd35d221f904b9e9f04d0
Max <example@gmail.com>
2021-01-08 17:34:00 +0500

    Module method

--- script.js
+++ script.js
@@ -1,78 +1,25 @@
 'use strict';
 
-// Инкапсуляция - отделение от внешнего мира внутренностей программы: переменных, функций и прочего
-// (в ООП - объект хранит свое состояние в приватном порядке, и только методы объекта имеют доступ для его изменения)
+const app = 123;
 
-// function User(name, age) {
-//     this.name = name;
-//     let userAge = age;
+const number = 1;
 
-//     this.say = function() {
-//         console.log(`Имя пользователя: ${this.name}, возраст: ${userAge}`);
-//     };
+(function(){            // Анонимная самовызывающаяся функция
+    let number = 2;     // Первый подход создания модуля
+    console.log(number);
+    console.log(number + 3);
+}());
 
-//     this.getAge = function() {
-//         return userAge;
-//     };
+console.log(number);
 
-//     this.setAge = function(age) {
-//         if (typeof age === 'number' && age > 0 && age < 110) {
-//             userAge = age;
-//         } else {
-//             console.log('Недопустимое значение!');
-//         }
-//     };
-// }
+const user = (function(){       // Второй подход
+    const privat = function() {
+        console.log('I am privat!');
+    };
 
-// const ivan = new User('Ivan', 27);
-// console.log(ivan.name);
-// console.log(ivan.getAge());
+    return {
+        sayHello: privat
+    };
+}());
 
-// ivan.setAge(30);
-// ivan.setAge(300);
-// console.log(ivan.getAge());
-
-// ivan.say();
-
-class User {
-    constructor(name, age) {
-        this.name = name;
-        this._Age = age;
-    }
-
-    #surname = 'Petrychenko'; // Экспериментальная возможность добавления свойств, приватные
-
-    say = () => {
-        console.log(`Имя пользователя: ${this.name} ${this.#surname}, возраст: ${this._age}`);
-    }
-
-    get age() {
-        return this._age;
-    }
-
-    set age(age) {
-        if (typeof age === 'number' && age > 0 && age < 110) {
-            this._age = age;
-        } else {
-            console.log('Недопустимое значение!');
-        }
-    }
-
-    get surname() {
-        return this.#surname;
-    }
-
-    set surname(surname) {
-        this.#surname = surname;
-    }
-}
-
-const ivan = new User('Ivan', 27);
-// console.log(ivan.age);
-// ivan.age = 99;
-// console.log(ivan.age);
-// console.log(ivan.surname);
-ivan.say();
-
-ivan.surname = 'Krabov';
-console.log(ivan.surname);
+user.sayHello();
